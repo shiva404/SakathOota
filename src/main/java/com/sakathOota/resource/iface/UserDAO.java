73a70bb8eb9425b4e1c2b21362a86ea419f2c8a5
shiva404 <shiva.n404@gmail.com>
2013-01-16 22:42:48 +0530

    -Restructuring the code

---
+++ src/main/java/com/sakathOota/resource/iface/UserDAO.java
@@ -0,0 +1,11 @@
+package com.sakathOota.resource.iface;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sn1
+ * Date: 1/15/13
+ * Time: 9:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserDAO {
+}
