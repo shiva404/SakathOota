24c08380fa561527687dff71e2c9a107652f3d60
shiva404 <shiva.n404@gmail.com>
2013-01-29 22:24:37 +0530

    -Adding database fields to the location table

--- sakathoota-ws/src/main/java/com/whiteSpace/resource/impl/FBRealTimeNotificationResourceImpl.java
+++ sakathoota-ws/src/main/java/com/whiteSpace/resource/impl/FBRealTimeNotificationResourceImpl.java
@@ -66,6 +66,7 @@ public class FBRealTimeNotificationResourceImpl implements FBRealTimeNotificatio
 		if(changedFiled.equals("checkins")){
 			Location location = userFacebookOperations.getLatestCheckin(fbNotification.getEntry().get(0).getUid().toString());
 			notification.setData(location.getUser().getName() + "cheked in to " + location.getName() +"," + location.getStreet() + "," + location.getCity());
+			logger.info("Sending data*******" + notification.getData());
 		}
 		return notification;
 	}
